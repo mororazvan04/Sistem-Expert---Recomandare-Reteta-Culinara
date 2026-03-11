69d9d1f4b5b96f5f304210fdf4ebcab9a8d5a80e
mororazvan04 <morosanu.razvan04@gmail.com>
2026-03-11 12:37:08 +0200

    Add files via upload

---
+++ prima parte.pl
@@ -0,0 +1,37 @@
+% =====================================================================
+% BAZA DE CUNOȘTINȚE - SISTEM EXPERT CULINAR
+% Autor: [Morosanu Razvan] (Membrul 1)
+% Descriere: Definește ingredientele disponibile, rețetele și atributele lor.
+% =====================================================================
+
+% 1. INGREDIENTE DISPONIBILE 
+ingredient(rosii). ingredient(castraveti). ingredient(ceapa). 
+ingredient(usturoi). ingredient(ardei). ingredient(cartofi). 
+ingredient(morcovi). ingredient(ciuperci). ingredient(salata). 
+ingredient(spanac). ingredient(mere). ingredient(lamai). 
+ingredient(pui). ingredient(vita). ingredient(porc). 
+ingredient(somon). ingredient(bacon). ingredient(oua). 
+ingredient(lapte). ingredient(branza_feta). ingredient(cascaval). 
+ingredient(parmezan). ingredient(unt). ingredient(smantana). 
+ingredient(paste). ingredient(orez). ingredient(faina). 
+ingredient(zahar). ingredient(sare). ingredient(piper). 
+ingredient(ulei_masline). ingredient(otet). ingredient(paine). 
+ingredient(pesmet).
+
+% 2. REȚETE ȘI INGREDIENTE NECESARE
+% Format: reteta(Nume, ListaIngrediente)
+reteta(omleta_simpla, [oua, sare, piper, unt]).
+reteta(omleta_cu_branza, [oua, cascaval, unt, sare]).
+reteta(salata_greceasca, [rosii, castraveti, ceapa, branza_feta, ulei_masline, sare]).
+reteta(salata_de_vara, [rosii, castraveti, ardei, ulei_masline, otet]).
+reteta(paste_carbonara, [paste, bacon, oua, parmezan, piper]).
+reteta(paste_cu_sos_rosii, [paste, rosii, usturoi, ulei_masline, sare]).
+reteta(pui_la_cuptor, [pui, cartofi, usturoi, ulei_masline, sare, piper]).
+reteta(pui_cu_ciuperci, [pui, ciuperci, smantana, usturoi, unt]).
+reteta(friptura_de_vita, [vita, unt, usturoi, sare, piper]).
+reteta(somon_la_gratar, [somon, lamai, ulei_masline, sare]).
+reteta(orez_cu_legume, [orez, morcovi, ceapa, ardei, ulei_masline]).
+reteta(cartofi_prajiti, [cartofi, ulei_masline, sare]).
+reteta(supa_crema_ciuperci, [ciuperci, ceapa, unt, smantana, sare]).
+reteta(snitel_pui, [pui, oua, faina, pesmet, ulei_masline]).
+reteta(clatite, [oua, faina, lapte, unt, zahar]).
