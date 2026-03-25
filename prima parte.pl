890a354e4b0532e93d61dbbdaccb58f06f295092
mororazvan04 <morosanu.razvan04@gmail.com>
2026-03-25 12:15:10 +0200

    Define 35 ingredients in prima parte.pl(Membru 1)
    Added definitions for 35 ingredients in Prolog.

--- prima parte.pl
+++ prima parte.pl
@@ -1,1 +1,10 @@
-
+%  1. Definiția ingredientelor (35 ingrediente)
+ingredient(rosii). ingredient(castraveti). ingredient(ceapa). ingredient(usturoi).
+ingredient(ardei). ingredient(cartofi). ingredient(morcovi). ingredient(ciuperci).
+ingredient(salata). ingredient(spanac). ingredient(mere). ingredient(lamai).
+ingredient(pui). ingredient(vita). ingredient(porc). ingredient(somon).
+ingredient(oua). ingredient(lapte). ingredient(branza_feta). ingredient(cascaval).
+ingredient(unt). ingredient(smantana). ingredient(paste). ingredient(orez).
+ingredient(faina). ingredient(zahar). ingredient(sare). ingredient(piper).
+ingredient(ulei_masline). ingredient(otet). ingredient(paine). ingredient(pesmet).
+ingredient(masline). ingredient(busuioc). ingredient(ton).
