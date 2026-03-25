74977adaa46ed96ea3e7803d1d5a3c68998544dc
mororazvan04 <morosanu.razvan04@gmail.com>
2026-03-25 12:15:48 +0200

    Add recipes and necessary ingredients
    Added 15 recipes with their required ingredients.

--- prima parte.pl
+++ prima parte.pl
@@ -8,3 +8,20 @@ ingredient(unt). ingredient(smantana). ingredient(paste). ingredient(orez).
 ingredient(faina). ingredient(zahar). ingredient(sare). ingredient(piper).
 ingredient(ulei_masline). ingredient(otet). ingredient(paine). ingredient(pesmet).
 ingredient(masline). ingredient(busuioc). ingredient(ton).
+
+%  2. Rețete și ingredientele necesare (15 rețete) 
+reteta(omleta_simpla, [oua, sare, piper, unt]).
+reteta(omleta_cu_branza, [oua, cascaval, unt, sare]).
+reteta(salata_greceasca, [rosii, castraveti, ceapa, masline, branza_feta, ulei_masline, sare]).
+reteta(salata_de_vara, [rosii, castraveti, ardei, ulei_masline, otet]).
+reteta(paste_carbonara, [paste, porc, oua, cascaval, piper]).
+reteta(paste_cu_sos_rosii, [paste, rosii, usturoi, ulei_masline, busuioc, sare]).
+reteta(pui_la_cuptor, [pui, cartofi, usturoi, ulei_masline, sare, piper]).
+reteta(pui_cu_ciuperci, [pui, ciuperci, smantana, usturoi, unt]).
+reteta(friptura_de_vita, [vita, unt, usturoi, sare, piper]).
+reteta(somon_la_gratar, [somon, lamai, ulei_masline, sare]).
+reteta(orez_cu_legume, [orez, morcovi, ceapa, ardei, ulei_masline]).
+reteta(cartofi_prajiti, [cartofi, ulei_masline, sare]).
+reteta(supa_crema_ciuperci, [ciuperci, ceapa, unt, smantana, sare]).
+reteta(snitel_pui, [pui, oua, faina, pesmet, ulei_masline]).
+reteta(clatite, [oua, faina, lapte, unt, zahar]).
